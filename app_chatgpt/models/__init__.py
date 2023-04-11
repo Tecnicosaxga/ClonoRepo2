b9b05fb5274cbd4c814fd8d9063595bf0f3d1e4a
Tecnicosaxga <128792387+Tecnicosaxga@users.noreply.github.com>
2023-04-11 12:50:38 +0200

    [ADD] app app_chatgpt

---
+++ app_chatgpt/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import mail_channel
+from . import res_config_settings
+from . import ai_robot
+from . import res_partner
+from . import res_users
+from . import mail_message
